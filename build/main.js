678629f429f6740a5fd6e63ed130bdedea5b4877
Carlos Nazareno <carlos.nazareno@incubeta.com>
2025-10-18 08:43:00 +0800

    added text

--- build/main.js
+++ build/main.js
@@ -5,8 +5,7 @@ let svg = body.append('svg').attr('width', svgWidth).attr('height', svgHeight)
     .attr('id', 'svgMain');
 
 let data = d3.json('data.json').then(function (data) {
-    svg.selectAll('circle').data(data).enter()
-        .append('circle')
+    svg.selectAll('circle').data(data).enter().append('circle')
         /*
         .attr('cx', function (d, i) => i * 50 + 20})
         */
@@ -14,6 +13,12 @@ let data = d3.json('data.json').then(function (data) {
         .attr('cy', d => d.y * 5)
         .attr('fill', d => d.color)
         .attr('r', d => d.value);
+
+        svg.selectAll('text').data(data).enter().append('text')
+        .attr('x', d => d.x * 5)
+        .attr('y', d => d.y * 5)
+        .text(d => d.color)
+        .attr('font-face', 'Arial, Helvetica, sans-serif')
 })
 
 console.log(svg);
